b8643917cd108187acd98f60ce025fedb43394e9
Puyma69 <puyma.amyup@gmail.com>
2019-05-26 19:20:18 +0200

    a

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var jqy = $.noConflict();
 
 jqy(document).ready(function(){
     jqy("button").click(function(){
-      jqy("#div1").load("https://puyma.xyz/src/lang.json");
+      jqy("#aoa").load("https://puyma.xyz/src/lang.json");
     });
   });
 
