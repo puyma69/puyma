c2cdf319ac0be63f58a29225a6f955e216ea4487
Puyma69 <puyma.amyup@gmail.com>
2019-05-26 18:46:09 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-JSON.parse();
+//jquery here ---------------------
+
+$(document).ready(function(){
+    $("button").click(function(){
+      $.getJSON("https://puyma.xyz/src/lang.json", function(result){
+        $.each(result, function(i, field){
+          $("div").append(field + " ");
+        });
+      });
+    });
+  });
+
+// js here ------------------------
